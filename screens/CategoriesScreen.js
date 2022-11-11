5366e3da173a4f061da746334cd55c78c391f012
rktdnjs <rktdnjs@naver.com>
2022-11-12 02:55:38 +0900

    95 error solved

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,19 +2,31 @@ import { CATEGORIES } from "../data/dummy-data";
 import { FlatList } from "react-native";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCateoryItem(itemData) { //선택사항, 단순히 JSX코드 길이를 줄이기 위함
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
-}
+function CategoriesScreen({ navigation }) {
+  
+  function renderCateoryItem(itemData) {
+    //선택사항, 단순히 JSX코드 길이를 줄이기 위함
+    function pressHandler() {
+        navigation.navigate("MealsOverview")
+    }
 
-function CategoriesScreen() {
     return (
-        <FlatList 
-        data={CATEGORIES} 
-        keyExtractor={(item) => item.id}
-        renderItem={renderCateoryItem} 
-        numColumns={2}
-        />
-    )
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCateoryItem}
+      numColumns={2}
+    />
+  );
 }
 
 export default CategoriesScreen;
