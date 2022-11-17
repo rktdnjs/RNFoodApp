f9658be5d0a8a4ecb51118e8360789559efe5fe9
rktdnjs <rktdnjs@naver.com>
2022-11-18 00:24:38 +0900

    100 save

---
+++ components/MealItem.js
@@ -0,0 +1,51 @@
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
+
+function MealItem({ title, imageUrl, duration, complexity, affordability }) {
+  return (
+    <View style={styles.mealItem}>
+      <Pressable  android_ripple={{color: '#ccc'}}>
+        <View>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <View style={styles.details}>
+          <Text style={styles.detailItem}>{duration}</Text>
+          <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default MealItem;
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius : 8,
+    overflow : 'hidden',
+    backgroundColor : 'white',
+    elevation : 4,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin : 8
+  },
+  details : {
+    flexDirection : 'row',
+    alignItems : 'center',
+    justifyContent : 'center',
+    padding : 8
+  },
+  detailItem : {
+    marginHorizontal : 4,
+    fontSize : 12
+  }
+});
